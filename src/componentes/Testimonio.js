8d093dfd3f519dcd20bdac79df98ba6e04c34d08
Jsson47 <martinezvalle98@gmail.com>
2025-03-23 21:44:16 -0600

    feat: initialize project structure with base directories

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio'
+      src={require('../imagenes/testimonio-emma.webp')}
+      alt="Foto de Emma"/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} in {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} at {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
